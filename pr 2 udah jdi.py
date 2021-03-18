2c57b03c6486a153ec2d0e9aa9286d71d7c8de23
beelebah27 <80262380+beelebah27@users.noreply.github.com>
2021-03-18 12:21:34 +0800

    Add files via upload

---
+++ pr 2 udah jdi.py
@@ -0,0 +1,7 @@
+print("NAMA : Rudi irawan \nNIM  : TI17200047 \nProdi: teknik informatiika (A)")
+
+def tinggi_gedung():
+    print("\ntinggi = 100m, naik = 5m, merosot = 4m\n")
+    print("\n jadi, untuk mencapai puncak membutuhkan",(5-4)*100),print("hari")
+
+tinggi_gedung()
